cf4b307eb7104ab088a4dd019485acf87c4fd0a8
Nicolas Jousset <joussetn49@gmail.com>
2020-01-08 14:38:23 +0100

    Nagui qui vole et easter egg

---
+++ Assets/Scripts/CameraMan.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMan : MonoBehaviour
+{
+
+    public GameObject naguiSuit;
+    public Material bleu;
+    public Material rouge;
+    public Material orange;
+    public Material peau;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+
+        if (collision.name == "Microphone Prefab(Clone)")
+        {
+            int rand = new System.Random().Next(1, 5);
+
+        switch (rand)
+        {
+            case 1:
+                naguiSuit.GetComponent<Renderer>().material = orange;
+                break;
+            case 2:
+                naguiSuit.GetComponent<Renderer>().material = bleu;
+                break;
+            case 3:
+                naguiSuit.GetComponent<Renderer>().material = rouge;
+                break;
+            case 4:
+                naguiSuit.GetComponent<Renderer>().material = peau;
+                break;
+            default:
+                break;
+        }
+
+
+        }
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Nagui.cs
+++ Assets/Scripts/Nagui.cs
@@ -28,12 +28,25 @@ public class Nagui : MonoBehaviour
 
             audience.transform.GetChild(randomChild).SetParent(transform.parent);
 
+            if(nbLifePoint == 0)
+            {
+                this.KillMe();
+            }
         }
         else
         {
             Debug.Log("collision name " + collision);
         }
     }
+
+
+    public void KillMe()
+    {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(new System.Random().Next(-10, 10), new System.Random().Next(15, 30), new System.Random().Next(-8, 8));
+        rb.AddTorque(new Vector3(new System.Random().Next(-100, 100), 0, new System.Random().Next(-100, 100)) * 150);
+    }
+
 
     // Update is called once per frame
     void Update()
