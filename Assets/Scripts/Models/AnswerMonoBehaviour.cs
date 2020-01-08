d54bcada7596c227298c00fcf4ce1e59745639c3
Gouin <christo.tpe@gmail.com>
2020-01-08 14:46:28 +0100

    ajout des musiques découpées

---
+++ Assets/Scripts/Models/AnswerMonoBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerMonoBehaviour : MonoBehaviour
+{
+    #region Fields
+    /// <summary>
+    /// Answer identifier
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// Answer to display
+    /// </summary>
+    public string Value { get; set; }
+    #endregion
+}
---
+++ Assets/Scripts/Models/Question.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{
+    #region Fields
+    /// <summary>
+    /// Question identifier
+    /// </summary>
+    public int Id { get; set; }
+    /// <summary>
+    /// Question to ask
+    /// </summary>
+    public string Value { get; set; }
+    /// <summary>
+    /// Possible answers
+    /// </summary>
+    public IEnumerable<AnswerMonoBehaviour> Answers { get; set; }
+    /// <summary>
+    /// Good answer to the question
+    /// </summary>
+    public AnswerMonoBehaviour GoodAnswer { get; set; }
+    /// <summary>
+    /// Identifier of the good answer
+    /// </summary>
+    public int GoodAnswerId { get; set; }
+    #endregion
+}
