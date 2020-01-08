21bb746a42758d156e7529141c6389edc77da2db
Nicolas Jousset <joussetn49@gmail.com>
2020-01-08 11:53:56 +0100

    Nagui et le public

---
+++ Assets/Scripts/MiiScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiiScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*
+     * void Start()
+    {
+        
+    }
+    */
+
+    public void KillMe()
+    {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(new System.Random().Next(-10, 10), new System.Random().Next(15, 30), new System.Random().Next(-8, 8));
+        rb.AddTorque(new Vector3(new System.Random().Next(-100, 100), 0, new System.Random().Next(-100, 100)) * 150);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Nagui.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nagui : MonoBehaviour
+{
+
+    public GameObject audience;
+    private int nbLifePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nbLifePoint = audience.transform.childCount;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+
+        if (collision.name == "Microphone Prefab(Clone)")
+        {
+
+            int randomChild = new System.Random().Next(0, audience.transform.childCount);
+
+            audience.transform.GetChild(randomChild).GetComponent<MiiScript>().KillMe();
+            nbLifePoint -= 1;
+            Debug.Log("nbLifePoint " + nbLifePoint);
+
+            audience.transform.GetChild(randomChild).SetParent(transform.parent);
+
+        }
+        else
+        {
+            Debug.Log("collision name " + collision);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
